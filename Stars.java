63fd7bb72c8359aa7cbd2fe4ae29cfe81916d002
Trent Butler <trentbutlerofficial@gmail.com>
2022-05-13 11:04:06 +1200

    added Path class

--- Stars.java
+++ Stars.java
@@ -2,13 +2,13 @@ import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Stars
  */
 public class Stars {
 
+    public Point goal;
     public static void main(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("Usage: java Stars <galaxy_csv_filename> <start_index> <end_index> <D>");
@@ -29,11 +29,60 @@ public class Stars {
         catch (Exception e) {
             e.printStackTrace();
         }
+        goal = points.get(points.size() - 1);
 
         return points;
     }
 
     private void search(ArrayList<Point> points) {
         
+    }
+
+    public class Path {
+        private ArrayList<Point> _points;
+        private double _heuristic;
+
+        public Path(Point first){
+            _points = new ArrayList<>();
+            _points.add(first);
+            _heuristic = getHeuristic(first);
+        }
+
+        public Path(Path path) {
+            _points = path.getList();
+            _heuristic = getHeuristic(_points.get(_points.size() - 1));
+        }
+
+        public ArrayList<Point> getList() {
+            return _points;
+        }
+
+        public double getHeuristic(Point currentPoint) {
+            return Math.sqrt((currentPoint.getY() - goal.getY()) * (currentPoint.getY() - goal.getY()) + (currentPoint.getX() - goal.getX()) * (currentPoint.getX() - goal.getX()));
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            Path clone = null;
+            try
+            {
+                clone = (Path) super.clone();
+                //Copy new List to cloned method
+                clone.setList((ArrayList<Point>) this.getList().clone());
+            } 
+            catch (CloneNotSupportedException e) 
+            {
+                e.printStackTrace();
+            }
+            return clone;
+        }
+
+        public void setList(ArrayList<Point> list) {
+            _points = list;
+        }
+
+        public void add(Point point){
+            _points.add(point);
+        }
     }
 }
